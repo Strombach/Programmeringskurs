45ff5d06a5299e9689ee5709ee43a448b3cf5931
strombach <fredde.stromback@gmail.com>
2024-10-03 13:42:35 +0200

    Adding function for submenu

--- Laboration_1/Lab1.py
+++ Laboration_1/Lab1.py
@@ -1,37 +1,50 @@
 import os
+import nmap
+import ipaddress
+
+scanner = nmap.PortScanner()
 
 ### Functions
-def Print_Menu():
+def Print_Main_Menu():
      userInput = False
      while not userInput:
           print("**** Menu ****")
-          print("1. Scan")
-          print("2. Open File")
-          print("3. Exit")
+          print("1. Enter IP:s manually")
+          print("2. Exit")
           try:
                user_choice = int(input("Enter a number: "))
                match user_choice:
                     case 1:
-                         print("Scan")
+                         print("Enter single or multiple IP:s to scan.")
+                         entered_ips = Print_Scan_Menu()
+                         Scan_all(entered_ips)
                     case 2:
-                         print("Open File")
-                    case 3:
                          print("Bye!")
                          quit()
+                    case _:
+                         raise ValueError
           except ValueError:
                os.system("clear")
                print("Not a valid number. Try again")
           else:
                userInput = True
-               print(user_choice)
 
+def Scan_all(ips_to_scan):
+     print(ips_to_scan)
+     # scanner.scan(hosts='10.10.10.0/24', arguments='-n -sP -PE -PA21,23,80,3389')
+     # hosts_list = [(x, scanner[x]['status']['state']) for x in scanner.all_hosts()]
+     # for host, status in hosts_list:
+     #      print(host, status)
 
-def Scan_all():
-     scanner = nmap.PortScanner()
+def Print_Scan_Menu():
+     os.system("clear")
+     userInput = False
+     while not userInput:
+          try:
+               ips=input("Enter the ips to scan with space between every ip:\n")
+               userInput = True
+               return ipaddress.ip_address(ips)
+          except:
+               print("Failed to read IP")
 
-     scanner.scan(hosts='10.10.10.0/24', arguments='-n -sP -PE -PA21,23,80,3389')
-     hosts_list = [(x, scanner[x]['status']['state']) for x in scanner.all_hosts()]
-     for host, status in hosts_list:
-          print(host, status)
-
-Print_Menu()
+Print_Main_Menu()
