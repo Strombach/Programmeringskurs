35c6b1ac2c95915ac4db59159dbe79b6645f4fb8
strombach <fredde.stromback@gmail.com>
2024-10-07 16:34:50 +0200

    Adding Menu for different kinds of scans

--- Laboration_1/Lab1.py
+++ Laboration_1/Lab1.py
@@ -5,7 +5,72 @@ import ipaddress
 scanner = nmap.PortScanner()
 
 ### Functions
+def Save_To_File(data):
+     print(data)
+
+def Scan_Ip(ips_to_scan, flags):
+     for ip in ips_to_scan:
+          ip_str = str(ip)
+          print(f"Scanning {ip_str}")
+          test = scanner.scan(hosts=ip_str, arguments=flags)
+          print(test)
+
+def Nmap_Scan_Menu(ips_to_scan):
+     os.system("clear")
+     userInput = False
+     while not userInput:
+          print("What type of scan?")
+          print("1. TCP Connect Scan (-sT)")
+          print("2. Stealth scan (-sS) !!REQUIRES ROOT!!")
+          print("3. Version scan (-sV)")
+          print("4. Enter own flags")
+          try:
+               user_choice = int(input("Enter a number: "))
+               match user_choice:
+                    case 1:
+                         flags = "-sT"
+                    case 2:
+                         flags = "-sS"
+                    case 3:
+                         flags = "-sV"
+                    # case 4:
+                    
+                    case _:
+                         raise ValueError
+               Scan_Ip(ips_to_scan, flags)
+          except ValueError:
+               os.system("clear")
+               print("Not a valid number. Try again")
+     for ip in ips_to_scan:
+          print(ip)
+
+
+
+def Print_Scan_Menu():
+     os.system("clear")
+     userInput = False
+     while not userInput:
+          ip_input = input("Enter the IP:s to scan with space between every IP or IP range (CIDR):\n")
+          input_list = ip_input.split(" ")
+          
+          ip_list = []
+          for ip in input_list:
+               try:
+                    if "/" in ip:
+                         new_range = ipaddress.ip_network(ip)
+                         ip_list.append(new_range)
+                    else:
+                         new_ip = ipaddress.ip_address(ip)
+                         ip_list.append(new_ip)
+               except ValueError as e:
+                    os.system("clear")
+                    print(f"{e} and won't be added to the list")
+          if len(ip_list) > 0:
+               userInput = True
+               return ip_list
+
 def Print_Main_Menu():
+     os.system("clear")
      userInput = False
      while not userInput:
           print("**** Menu ****")
@@ -17,7 +82,7 @@ def Print_Main_Menu():
                     case 1:
                          print("Enter single or multiple IP:s to scan.")
                          entered_ips = Print_Scan_Menu()
-                         Scan_all(entered_ips)
+                         Nmap_Scan_Menu(entered_ips)
                     case 2:
                          print("Bye!")
                          quit()
@@ -29,38 +94,5 @@ def Print_Main_Menu():
           else:
                userInput = True
 
-def Scan_all(ips_to_scan):
-     for ip in ips_to_scan:
-          ip_str = str(ip)
-          print(f"Scanning {ip_str}")
-          scanner.scan(hosts=ip_str, arguments='-n -sP -PE -PA21,23,80,3389')
-          hosts_list = [(x, scanner[x]['status']['state']) for x in scanner.all_hosts()]
-          for host, status in hosts_list:
-               print(host, status)
-
-
-def Print_Scan_Menu():
-     os.system("clear")
-     userInput = False
-     while not userInput:
-          ip_input = input("Enter the IP:s to scan with space between every IP OR one IP range (CIDR):\n")
-          input_list = ip_input.split(" ")
-          
-          ip_list = []
-          for ip in input_list:
-               try:
-                    if "/" in ip:
-                         new_range = ipaddress.ip_network(ip)
-                         ip_list.append(new_range)
-                         break
-                    else:
-                         new_ip = ipaddress.ip_address(ip)
-                         ip_list.append(new_ip)
-               except ValueError as e:
-                    print(f"{e} and won't be added to the list")
-
-          if len(ip_list) > 0:
-               userInput = True
-               return ip_list
-
+### Script
 Print_Main_Menu()
