1a495fb710771f00fa8cee7838f023b921fe3b32
strombach <fredde.stromback@gmail.com>
2024-10-18 18:14:02 +0200

    Adding error handling for file not found

--- Laboration_2/Lab2.py
+++ Laboration_2/Lab2.py
@@ -11,5 +11,20 @@ def encrypt_file(file, key):
 def decrypt_file(file, key):
     print(f"Decrypt: {file} with {key}")
 
+def main():
+    parser = argparse.ArgumentParser(description="Crypto tool", usage='%(prog)s [options]')
+    parser.add_argument("-e", "--encrypt", action="store_true", help="If the script should encrypt a file.")
+    parser.add_argument("-d", "--decrypt", action="store_true", help="If the script should decrypt a file.")
+    parser.add_argument("-f", "--file", help="The file to encrypt/decrypt with.")
+    args = parser.parse_args()
+
+    if not args.file:
+        print("You need to use a file to encrypt/decrypt a file.")
+    else:
+        try:
+            file = open(args.file, "rb")
+        except FileNotFoundError:
+            print("The file couldn't be found.")
+
 if __name__ == "__main__":
     main()
