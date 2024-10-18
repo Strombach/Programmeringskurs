0f576aafdc21541a769701f94f4873eb41865803
Fredrik Strömbäck <fredde.stromback@gmail.com>
2024-10-18 16:07:04 +0200

    Creating small foundation

--- Laboration_2/Lab2.py
+++ Laboration_2/Lab2.py
@@ -1,3 +1,15 @@
+import argparse
 import generate_key
 
-generate_key.generate_key()
+def generate_new_key():
+    generate_key.generate_key()
+    print("Generating new key")
+
+def encrypt_file(file, key):
+    print(f"Encrypt: {file} with {key}")
+
+def decrypt_file(file, key):
+    print(f"Decrypt: {file} with {key}")
+
+if __name__ == "__main__":
+    main()
