d5b91fb0fd1b8c5b7c3440f8876f3e3563cd496e
strombach <fredde.stromback@gmail.com>
2024-10-21 16:37:52 +0200

    Script now saves decrypted data to file

--- Laboration_2/Lab2.py
+++ Laboration_2/Lab2.py
@@ -7,11 +7,16 @@ def encrypt_data(data, key):
     
     cipher_suite = Fernet(key)
 
-    cipher_data =  cipher_suite.encrypt(data)
+    cipher_data = cipher_suite.encrypt(data.encode())
     return cipher_data
 
-def decrypt_file(file, key):
-    print(f"Decrypt: {file} with {key}")
+def decrypt_data(data, key):
+    print("Decrypting data...")
+
+    cipher_suite = Fernet(key)
+
+    plain_data = cipher_suite.decrypt(data)
+    return plain_data
 
 def main():
     parser = argparse.ArgumentParser(description="Crypto tool", usage='%(prog)s -f [file to encrypt or decrypt] [options]')
@@ -24,43 +29,57 @@ def main():
     parser.add_argument("-e", "--encrypt", action="store_false", help="If the script should encrypt a file.")
     
     # Optional
-    parser.add_argument("-k", "--key", help="The keyfile to encrypt/decrypt with.")
-    parser.add_argument("-d", "--decrypt", action="store_true", help="If the script should decrypt a file.")
+    parser.add_argument("-k", "--key", help="If you got a key file to encrypt with, use this flag.")
+    parser.add_argument("-dk", "--decryptkey", help="Key file to decrypt with.")
     
     args = parser.parse_args()
 
     # Finding "content file".
     try:
-        with open(args.file, "rb") as content_file:
-            content = content_file.read().strip()
+        if args.decryptkey:
+            with open(args.file, "rb") as content_file:
+                content = content_file.read()
+        else:    
+            with open(args.file, "r") as content_file:
+                content = content_file.read()
     except FileNotFoundError:
         print("The file to encrypt/decrypt couldn't be found")
         exit()
 
     # Create or find the key.
     try:
-        if args.newkey and not args.key:
+        if args.newkey and not args.key and not args.decryptkey:
             key_name = input("Name the key (Default [secret].key): ")
             print("Generating new key.")
             key_file = key_generator.generate_key(key_name)
         else:
-            key_file = args.key
+            if args.key or args.decryptkey:
+                key_file = args.key or args.decryptkey
+            else:
+                raise Exception("Need to enter a key file (-k, --key).")
 
         with open(key_file, "rb") as key_file:
             key = key_file.read()
     except FileNotFoundError:
         print("The key file couldn't be found.")
         exit()
+    except Exception as err:
+        print(err)
+        exit()
 
     try:
-        if args.encrypt and not args.decrypt:
+        if args.encrypt and not args.decryptkey:
             encrypted_data = encrypt_data(content, key)
 
             with open(f"{args.file}.enc", "wb") as encrypted_file:
                 encrypted_file.write(encrypted_data)
         else:
-            decrypt_file(content, key)
-    except Exception:
+            decrypted_data = decrypt_data(content, key)
+
+            with open(f"decrypted_data.txt", "w") as decrypted_file:
+                decrypted_file.write(decrypted_data.decode())
+    except Exception as err:
+        print(err)
         print("Couldn't save to file")
 
 if __name__ == "__main__":
