e011cef778192a831b1541585deef4f766d5bd72
strombach <fredde.stromback@gmail.com>
2024-10-21 13:54:44 +0200

    Now key_generator is a stand alone script

--- Laboration_2/key_generator.py
+++ Laboration_2/key_generator.py
@@ -1,14 +1,18 @@
 from cryptography.fernet import Fernet
 
 def generate_key(name = None):
-    key_name = "secret"
     key = Fernet.generate_key()
     
     if name:
         key_name = name
+    else:
+        key_name = "secret"
     
     with open(f"{key_name}.key", "wb") as key_file:
         key_file.write(key)
     print(f"{key_name}.key is now created and saved.")
 
     return f"{key_name}.key"
+
+if __name__ == "__main__":
+    generate_key()
