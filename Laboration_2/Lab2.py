96680ca16f938dd6fdb1bbb703a21297a2e2d8f5
strombach <fredde.stromback@gmail.com>
2024-10-21 14:07:01 +0200

    Refactoring

--- Laboration_2/Lab2.py
+++ Laboration_2/Lab2.py
@@ -10,29 +10,32 @@ def decrypt_file(file, key):
 def main():
     parser = argparse.ArgumentParser(description="Crypto tool", usage='%(prog)s [options]')
     
-    parser.add_argument("-f", "--file", help="The file to encrypt or decrypt.")
+    # Mandatory flag
+    parser.add_argument("-f", "--file", help="The file to encrypt or decrypt.", required=True)
+    
+    # Default options
+    parser.add_argument("-nk", "--newkey", action="store_false", help="Create a new file so encrypt/decrypt other files with.")
+    parser.add_argument("-e", "--encrypt", action="store_false", help="If the script should encrypt a file.")
+    
+    # Optional
     parser.add_argument("-k", "--key", help="The keyfile to encrypt/decrypt with.")
-    parser.add_argument("-e", "--encrypt", action="store_true", help="If the script should encrypt a file.")
     parser.add_argument("-d", "--decrypt", action="store_true", help="If the script should decrypt a file.")
-    parser.add_argument("-nk", "--newkey", action="store_true", help="Create a new file so encrypt/decrypt other files with.")
     
     args = parser.parse_args()
-    
-    if not args.key and not args.newkey:
-        print("You need to use a file to encrypt/decrypt a file.")
-    else:
-        try:
-            if args.newkey:
-                key_name = input("Name the key (Default [secret].key): ")
-                print("Generating new key.")
-                key_file = key_generator.generate_key(key_name)
-            else:
-                key_file = args.key
 
-            with open(key_file, "rb") as key_file:
-                key = key_file.read()
-        except FileNotFoundError:
-            print("The file couldn't be found.")
+    try:
+        if args.newkey and not args.key:
+            key_name = input("Name the key (Default [secret].key): ")
+            print("Generating new key.")
+            key_file = key_generator.generate_key(key_name)
+        else:
+            key_file = args.key
+        with open(key_file, "rb") as key_file:
+            key = key_file.read()
+    except FileNotFoundError:
+        print("The file couldn't be found.")
+    
+    print(key)
 
 if __name__ == "__main__":
     main()
