cb03572b1c41c38ca9fc6dd54174023012fa4121
strombach <fredde.stromback@gmail.com>
2024-10-29 09:24:25 +0100

    Start adding main menu

--- Projekt/hacker_tools.py
+++ Projekt/hacker_tools.py
@@ -1,7 +1,23 @@
+import os
 from toolbox import crypto_tool
 from toolbox import domenus
 
+while True:
+    os.system('clear')
+    print("""
+    Hacker Tools
+    1. Crypto Tool
+    2. DomEnus
+    0. Exit
+    """)
+    menu_choice = input("What app to use?: ")
 
-crypto_tool.crypto_tool()
-
-# domenus.main()
+    match(menu_choice):
+        case("1"):
+            crypto_tool.crypto_tool()
+        case("2"):
+            domenus.main()
+        case("0"):
+            exit()
+        case _:
+            print("Not a valid option.")
