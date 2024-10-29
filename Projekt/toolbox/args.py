50a8ead37311410e85da605ed252a8fa5546e8a9
strombach <fredde.stromback@gmail.com>
2024-10-29 17:53:41 +0100

    Intorducing validation to DomEnus setters.

--- Projekt/toolbox/args.py
+++ Projekt/toolbox/args.py
@@ -1,6 +1,8 @@
 # A file to keep all the args-classes needed to run tools in the main script.
 #TODO: For better code
 
+import validators
+
 class Crypto_Args:
     def __init__(self, file="", decryptkey=None, encrypt=True, newkey=True, key=None):
         self.file = file
@@ -13,19 +15,36 @@ class Domenus_Args:
 
     def __init__(self):
         self.domain = self.set_domain()
-        self.threads = self.set_threads
-        self.ports = self.set_ports,
-        self.silent = self.set_silent
-        self.verbose = self.set_verbose
-        self.savefile = self.set_savefile
-        self.enable_bruteforce = self.set_enable_bruteforce
-        self.engines = self.set_engines
+        self.threads = self.set_threads()
+        self.ports = self.set_ports(),
+        self.silent = self.set_silent()
+        self.verbose = self.set_verbose()
+        self.savefile = self.set_savefile()
+        self.enable_bruteforce = self.set_enable_bruteforce()
+        self.engines = self.set_engines()
 
     def set_domain(self):
-        return "example.com"
+        while True:
+            domain = input("Enter a domain: ").strip()
+
+            if not validators.domain(domain):
+                print("Not a valid domain")
+                continue
+            else:
+                return domain
 
     def set_threads(self):
-        return 20
+        DEFAULT_THREADS = 10
+        while True:
+            try:
+                threads = input(f"Enter number of threads (Leave empty for {DEFAULT_THREADS}): ").strip()
+
+                if threads == "":
+                    return DEFAULT_THREADS
+                else:
+                    return int(threads)
+            except ValueError:
+                print("Not a number...")
 
     def set_ports(self):
         return None
