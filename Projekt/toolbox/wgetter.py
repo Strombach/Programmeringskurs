5d2896c8d34b43cb66b7ce840655e3e1c40ef829
strombach <fredde.stromback@gmail.com>
2024-10-31 16:52:19 +0100

    Wgetter now can download every image from a website and save to file

--- Projekt/toolbox/wgetter.py
+++ Projekt/toolbox/wgetter.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
 
 HELP_STRING = """
         Wgetter
@@ -16,8 +17,29 @@ def main(flags):
 
         soup = BeautifulSoup(response.content, "html.parser")
 
-        with open("downloaded_page.html", "w", encoding="utf-8") as file:
-            file.write(soup.prettify())
+        with open("downloaded_page.html", "w", encoding="utf-8") as html_file:
+            html_file.write(soup.prettify())
+
+        os.makedirs("downloaded_assets")
+
+        for tag in soup.find_all("img"):
+            asset_url = tag.get("src")
+            print(asset_url)
+
+            if asset_url:
+                asset_url = urljoin(flags.url, asset_url)
+                asset_name = os.path.basename(urlparse(asset_url).path)
+                print(asset_url)
+                print(asset_name)
+
+                asset = requests.get(asset_url)
+                if asset.status_code == 200:
+                    print("Found asset")
+                    with open(f"downloaded_assets/{asset_name}", "wb") as asset_file:
+                        asset_file.write(asset.content)
+                    print(f"Downloaded asset: {asset_name}")
+                else:
+                    print(f"Failed to find: {asset_url}")
     else:
         print(f"Can't reach {flags.url}.")
 
