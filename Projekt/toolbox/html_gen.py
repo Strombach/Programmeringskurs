b44b99fe5ee36911adcd1cf95032fd78a238bf48
strombach <fredde.stromback@gmail.com>
2024-11-01 10:23:02 +0100

    Now a script tag is added to the  html and saved to new file

--- Projekt/toolbox/html_gen.py
+++ Projekt/toolbox/html_gen.py
@@ -1,13 +1,20 @@
 from bs4 import BeautifulSoup
 
 def main():
-    with open("index.html", "r", encoding="utf-8") as file:
-        html_content = file.read()
+    with open("index.html", "r", encoding="utf-8") as original_file:
+        html_content = original_file.read()
 
     soup = BeautifulSoup(html_content, "html.parser")
 
     title = soup.title.string
     print(f"Title of the page: {title}")
+
+    new_script = soup.new_tag("script")
+    new_script.string = "console.log('Added a new script')"
+    soup.body.append(new_script)
+
+    with open("modified_index.html", "w", encoding="utf-8") as new_file:
+        new_file.write(str(soup.prettify()))
 
 if __name__ == "__main__":
     main()
